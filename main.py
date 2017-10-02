e5c4c910d61155b72443599f0d73823b03ca6260
RenanLeviathan <renan.leviathan@gmail.com>
2017-10-02 10:43:17 -0300

    Alterações definitivas para a versão 1.0

--- main.py
+++ main.py
@@ -7,11 +7,18 @@ from tag import Tag
 from csv_reader import *
 r = Reader()
 ano = input("Ano:")
+cons=input("Conselho:")
 nome = input("Arquivo de entrada:")
 data = r.get_data(nome)
 output = input("Arquivo de saída:")
 arq = open(output,'w')
+arq.write('''
+<tr>
+             <td>&nbsp;</td>
+             <td colspan="2" class="text2">{}</td>
+           </tr>
+'''.format(ano))
 for d in data:
-	t = Tag(d[0],d[1],d[2],ano)
+	t = Tag(cons,d[0],d[1],d[2],ano)
 	arq.write(t.get_html())
 arq.close()
--- tag.py
+++ tag.py
@@ -1,7 +1,8 @@
 class Tag:
-	def __init__(self,link,title,content,year):
+	def __init__(self,cons,link,title,content,year):
 		self.link=link
 		self.title=title
+		self.cons=cons
 		self.content=content
 		self.year=year
 	
@@ -10,11 +11,11 @@ class Tag:
 		<tr>
              <td>&nbsp;</td>
              <td class="text2"><img src="../imagens/marcadores/seta.png" alt="seta" width="10" height="10" /></td>
-             <td class="text2"><span class="conteudo_interno"><a href="downloads/resolucoes/consun/{}/{}">{}</a></span></td>
+             <td class="text2"><span class="conteudo_interno"><a href="downloads/resolucoes/{}/{}/{}">{}</a></span></td>
            </tr>
            <tr>
              <td>&nbsp;</td>
              <td class="text2">&nbsp;</td>
              <td class="conteudo_interno">{}</td>
              </tr>
-		'''.format(self.year,self.link,self.title,self.content)
+		'''.format(self.cons,self.year,self.link,self.title,self.content)
